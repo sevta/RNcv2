618bab1859fa843989aacb3e8061ab47908abd5a
sevta <sevtasaputra@gmail.com>
2020-04-21 14:40:48 +0700

    initial commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,54 +1,146 @@
-import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import * as WebBrowser from "expo-web-browser";
+import * as React from "react";
+import {
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import layout from "../constants/Layout";
 
-import { MonoText } from '../components/StyledText';
-
+import {
+  Container,
+  Header,
+  Body,
+  Grid,
+  Title,
+  Left,
+  Right,
+  Col,
+  H1,
+  Card,
+  CardItem,
+  Content,
+  H3,
+  Thumbnail,
+} from "native-base";
+import { MonoText } from "../components/StyledText";
+import Carousel from "react-native-snap-carousel";
 export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
+  const carouselRef = React.useRef();
+  const [slides, setSlides] = React.useState([
+    {
+      label: "a",
+    },
+    {
+      label: "a",
+    },
+    {
+      label: "a",
+    },
+    {
+      label: "a",
+    },
+  ]);
+
+  React.useEffect(() => {
+    console.log("window", layout);
+  });
+
+  function renderItem({ item, index }) {
+    return (
+      <Card>
+        <CardItem cardBody style={{ overflow: "hidden" }}>
           <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
+            style={{ width: layout.window.width, height: 300 }}
+            source={{
+              uri:
+                "https://images.unsplash.com/photo-1558980664-1db506751c6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+            }}
           />
+        </CardItem>
+        <CardItem>
+          <H3 style={{ paddingVertical: 20 }}>Hellow</H3>
+        </CardItem>
+      </Card>
+    );
+  }
+
+  return (
+    <Container>
+      <Header
+        noShadow
+        style={{ backgroundColor: "deepskyblue" }}
+        androidStatusBarColor="deepskyblue"
+      >
+        <Body>
+          <Title style={{ paddingLeft: 16 }}>Covid19</Title>
+        </Body>
+      </Header>
+      <Content style={{ flex: 1, paddingTop: 20 }}>
+        <Grid>
+          <Col style={{ alignItems: "center", justifyContent: "center" }}>
+            <Carousel
+              loop
+              layout={"default"}
+              ref={carouselRef}
+              data={slides}
+              sliderWidth={layout.window.width}
+              itemWidth={layout.window.width - 32}
+              renderItem={renderItem}
+            />
+          </Col>
+        </Grid>
+        <View style={{ marginTop: 12 }}>
+          <BadgeList />
+          <BadgeList />
+          <BadgeList />
         </View>
+      </Content>
+    </Container>
+  );
+}
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Open up the code for this screen:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change any of the text, save the file, and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-        <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>navigation/BottomTabNavigator.js</MonoText>
-        </View>
-      </View>
-    </View>
+function BadgeList() {
+  return (
+    <Grid>
+      <Col style={{ paddingHorizontal: 16 }}>
+        <Card transparent={true}>
+          <CardItem
+            style={{ borderRadius: 8, backgroundColor: "whitesmoke" }}
+            bordered={false}
+          >
+            <Body>
+              <View>
+                <Text
+                  style={{
+                    textTransform: "uppercase",
+                    fontSize: 16,
+                    fontWeight: "bold",
+                    color: "#222",
+                  }}
+                >
+                  data terkini di indonesia
+                </Text>
+                <Text
+                  style={{
+                    textTransform: "uppercase",
+                    color: "#222",
+                    opacity: 0.5,
+                    marginTop: 4,
+                  }}
+                >
+                  2020-04-14
+                </Text>
+              </View>
+            </Body>
+          </CardItem>
+        </Card>
+      </Col>
+    </Grid>
   );
 }
 
@@ -56,95 +148,62 @@ HomeScreen.navigationOptions = {
   header: null,
 };
 
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use useful development
-        tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   developmentModeText: {
     marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
+    color: "rgba(0,0,0,0.4)",
     fontSize: 14,
     lineHeight: 19,
-    textAlign: 'center',
+    textAlign: "center",
   },
   contentContainer: {
     paddingTop: 30,
   },
   welcomeContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 10,
     marginBottom: 20,
   },
   welcomeImage: {
     width: 100,
     height: 80,
-    resizeMode: 'contain',
+    resizeMode: "contain",
     marginTop: 3,
     marginLeft: -10,
   },
   getStartedContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     marginHorizontal: 50,
   },
   homeScreenFilename: {
     marginVertical: 7,
   },
   codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
+    color: "rgba(96,100,109, 0.8)",
   },
   codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
+    backgroundColor: "rgba(0,0,0,0.05)",
     borderRadius: 3,
     paddingHorizontal: 4,
   },
   getStartedText: {
     fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
+    color: "rgba(96,100,109, 1)",
     lineHeight: 24,
-    textAlign: 'center',
+    textAlign: "center",
   },
   tabBarInfoContainer: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
     left: 0,
     right: 0,
     ...Platform.select({
       ios: {
-        shadowColor: 'black',
+        shadowColor: "black",
         shadowOffset: { width: 0, height: -3 },
         shadowOpacity: 0.1,
         shadowRadius: 3,
@@ -153,27 +212,27 @@ const styles = StyleSheet.create({
         elevation: 20,
       },
     }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
+    alignItems: "center",
+    backgroundColor: "#fbfbfb",
     paddingVertical: 20,
   },
   tabBarInfoText: {
     fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
+    color: "rgba(96,100,109, 1)",
+    textAlign: "center",
   },
   navigationFilename: {
     marginTop: 5,
   },
   helpContainer: {
     marginTop: 15,
-    alignItems: 'center',
+    alignItems: "center",
   },
   helpLink: {
     paddingVertical: 15,
   },
   helpLinkText: {
     fontSize: 14,
-    color: '#2e78b7',
+    color: "#2e78b7",
   },
 });
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,53 +1,347 @@
-import { Ionicons } from '@expo/vector-icons';
-import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import { Ionicons } from "@expo/vector-icons";
+import * as WebBrowser from "expo-web-browser";
+import * as React from "react";
+import { StyleSheet, Text, View, Switch } from "react-native";
+import { RectButton, ScrollView } from "react-native-gesture-handler";
+import {
+  Content,
+  Container,
+  Header,
+  Body,
+  Title,
+  Right,
+  Icon,
+  Button,
+  List,
+  ListItem,
+  H3,
+  Left,
+  Tabs,
+  Tab,
+  TabHeading,
+  H2,
+  Grid,
+  Col,
+  Spinner,
+} from "native-base";
+
+const colorPalette = {
+  dark1: "#263238",
+};
+
+const API = "https://api.covid19api.com/summary";
+
+const isThemeLight = (theme) => theme == "light";
 
 export default function LinksScreen() {
+  const [coronaSummary, setCoronaSummary] = React.useState([]);
+  const [coronaGlobal, setCoronaGlobal] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+  const [theme, setTheme] = React.useState("light");
+
+  React.useEffect(() => {
+    getCoronaData();
+  }, []);
+
+  function getCoronaData() {
+    fetch(API)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCoronaSummary(data);
+        setCoronaGlobal(data.Global);
+      })
+      .catch((err) => console.log("err", err))
+      .finally(() =>
+        setTimeout(() => {
+          setLoading(false);
+        }, 3000)
+      );
+  }
+
+  function onSwitchDarkMode(darkMode) {
+    darkMode ? setTheme("dark") : setTheme("light");
+  }
+
   return (
-    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-      <OptionButton
-        icon="md-school"
-        label="Read the Expo documentation"
-        onPress={() => WebBrowser.openBrowserAsync('https://docs.expo.io')}
-      />
-
-      <OptionButton
-        icon="md-compass"
-        label="Read the React Navigation documentation"
-        onPress={() => WebBrowser.openBrowserAsync('https://reactnavigation.org')}
-      />
-
-      <OptionButton
-        icon="ios-chatboxes"
-        label="Ask a question on the forums"
-        onPress={() => WebBrowser.openBrowserAsync('https://forums.expo.io')}
-        isLastOption
-      />
-    </ScrollView>
+    <Container>
+      <Header
+        hasTabs
+        style={{
+          backgroundColor: isThemeLight(theme)
+            ? "whitesmoke"
+            : colorPalette.dark1,
+        }}
+        androidStatusBarColor={
+          theme == "light" ? "whitesmoke" : colorPalette.dark1
+        }
+        noShadow
+      >
+        <Body>
+          <Title
+            style={{
+              paddingLeft: 10,
+              color: theme == "light" ? "#333" : "#fff",
+            }}
+          >
+            Cegah Covid 19
+          </Title>
+        </Body>
+        <Right>
+          <Button transparent>
+            <Icon
+              name="menu"
+              style={{
+                color: isThemeLight(theme) ? colorPalette.dark1 : "#fff",
+              }}
+            ></Icon>
+          </Button>
+        </Right>
+      </Header>
+      <Tabs
+        tabContainerStyle={{ elevation: 0 }}
+        tabBarPosition="bottom"
+        tabBarUnderlineStyle={{
+          backgroundColor: isThemeLight(theme) ? colorPalette.dark1 : "#fff",
+        }}
+      >
+        <Tab
+          heading={
+            <TabHeading
+              style={{
+                backgroundColor: isThemeLight(theme)
+                  ? "whitesmoke"
+                  : colorPalette.dark1,
+                borderColor: colorPalette.dark1,
+              }}
+              // activeTabStyle={{ backgroundColor: colorPalette.dark1 }}
+            >
+              <Text
+                style={{
+                  color: isThemeLight(theme) ? colorPalette.dark1 : "#fff",
+                }}
+              >
+                Home
+              </Text>
+            </TabHeading>
+          }
+        >
+          <Tab1 loading={loading} theme={theme} />
+        </Tab>
+        <Tab
+          heading={
+            <TabHeading
+              style={{
+                backgroundColor: isThemeLight(theme)
+                  ? "whitesmoke"
+                  : colorPalette.dark1,
+              }}
+            >
+              <Text
+                style={{
+                  color: isThemeLight(theme) ? colorPalette.dark1 : "#fff",
+                }}
+              >
+                Statistik
+              </Text>
+            </TabHeading>
+          }
+        >
+          <Tab2
+            coronaGlobal={coronaGlobal}
+            coronaSummary={coronaSummary}
+            loading={loading}
+            theme={theme}
+          />
+        </Tab>
+        <Tab
+          heading={
+            <TabHeading
+              style={{
+                backgroundColor: isThemeLight(theme)
+                  ? "whitesmoke"
+                  : colorPalette.dark1,
+              }}
+            >
+              <Text
+                style={{
+                  color: isThemeLight(theme) ? colorPalette.dark1 : "#fff",
+                }}
+              >
+                Berita
+              </Text>
+            </TabHeading>
+          }
+        >
+          <Tab3 theme={theme} onSwitchDarkMode={onSwitchDarkMode} />
+        </Tab>
+      </Tabs>
+    </Container>
   );
 }
 
-function OptionButton({ icon, label, onPress, isLastOption }) {
+function Tab1({ loading, theme }) {
   return (
-    <RectButton style={[styles.option, isLastOption && styles.lastOption]} onPress={onPress}>
-      <View style={{ flexDirection: 'row' }}>
-        <View style={styles.optionIconContainer}>
-          <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
-        </View>
-        <View style={styles.optionTextContainer}>
-          <Text style={styles.optionText}>{label}</Text>
-        </View>
-      </View>
-    </RectButton>
+    <Content
+      style={{
+        flex: 1,
+        backgroundColor: theme == "light" ? "#fff" : colorPalette.dark1,
+      }}
+    >
+      {loading ? (
+        <Spinner />
+      ) : (
+        <List>
+          {Array(30)
+            .fill("")
+            .map((_, i) => (
+              <ListItem
+                noBorder
+                thumbnail
+                key={i}
+                button
+                itemDivider={false}
+                selected
+                style={{
+                  borderBottomWidth: 1,
+                  borderBottomColor: "rgba(38,50,56,1)",
+                }}
+              >
+                <Left>
+                  <View
+                    style={{
+                      width: 60,
+                      height: 60,
+                      backgroundColor: isThemeLight(theme)
+                        ? colorPalette.dark1
+                        : "#fff",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <H3
+                      style={{
+                        color: isThemeLight(theme)
+                          ? "#fff"
+                          : colorPalette.dark1,
+                      }}
+                    >
+                      01
+                    </H3>
+                  </View>
+                </Left>
+                <Body>
+                  <Text
+                    style={{
+                      fontSize: 18,
+                      color: isThemeLight(theme) ? colorPalette.dark1 : "#fff",
+                    }}
+                  >
+                    Title
+                  </Text>
+                  <Text
+                    style={{
+                      fontSize: 14,
+                      fontWeight: "600",
+                      color: isThemeLight(theme) ? colorPalette.dark1 : "#fff",
+                    }}
+                  >
+                    loremlorem loremlorem loremlorem{" "}
+                  </Text>
+                </Body>
+                <Right></Right>
+              </ListItem>
+            ))}
+        </List>
+      )}
+    </Content>
   );
 }
+
+function Tab2({ coronaGlobal, coronaSummary, loading, theme }) {
+  return (
+    <Content
+      style={{
+        flex: 1,
+        backgroundColor: isThemeLight(theme) ? "#fff" : colorPalette.dark1,
+        padding: 16,
+      }}
+    >
+      <Grid>
+        <Col>
+          {loading ? (
+            <Spinner />
+          ) : (
+            <View
+              style={{
+                backgroundColor: "whitesmoke",
+                paddingHorizontal: 20,
+                paddingVertical: 14,
+                borderRadius: 12,
+              }}
+            >
+              <Text style={{ color: "#333", fontSize: 16, marginVertical: 4 }}>
+                Total confirmed {coronaGlobal.TotalConfirmed}
+              </Text>
+              <Text style={{ color: "#333", fontSize: 16, marginVertical: 4 }}>
+                Total death {coronaGlobal.TotalDeaths}
+              </Text>
+              <Text style={{ color: "#333", fontSize: 16, marginVertical: 4 }}>
+                Total recovered {coronaGlobal.TotalRecovered}
+              </Text>
+            </View>
+          )}
+        </Col>
+      </Grid>
+    </Content>
+  );
+}
+
+function Tab3({ theme, onSwitchDarkMode }) {
+  const [darkMode, setDarkMode] = React.useState(!isThemeLight(theme));
+
+  React.useEffect(() => {
+    onSwitchDarkMode(darkMode);
+  }, [darkMode]);
+
+  function toggleSwitch() {
+    setDarkMode((prev) => !prev);
+  }
+
+  return (
+    <Content
+      style={{
+        flex: 1,
+        backgroundColor: isThemeLight(theme) ? "#fff" : colorPalette.dark1,
+      }}
+    >
+      <List>
+        <ListItem>
+          <Body>
+            <Text
+              style={{
+                color: isThemeLight(theme) ? colorPalette.dark1 : "#fff",
+              }}
+            >
+              Dark mode
+            </Text>
+          </Body>
+          <Right>
+            <Switch value={darkMode} onValueChange={toggleSwitch} />
+          </Right>
+        </ListItem>
+      </List>
+    </Content>
+  );
+}
+
+LinksScreen.navigationOptions = {
+  tabBarVisible: false,
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fafafa',
+    backgroundColor: "#fafafa",
   },
   contentContainer: {
     paddingTop: 15,
@@ -56,19 +350,19 @@ const styles = StyleSheet.create({
     marginRight: 12,
   },
   option: {
-    backgroundColor: '#fdfdfd',
+    backgroundColor: "#fdfdfd",
     paddingHorizontal: 15,
     paddingVertical: 15,
     borderWidth: StyleSheet.hairlineWidth,
     borderBottomWidth: 0,
-    borderColor: '#ededed',
+    borderColor: "#ededed",
   },
   lastOption: {
     borderBottomWidth: StyleSheet.hairlineWidth,
   },
   optionText: {
     fontSize: 15,
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start",
     marginTop: 1,
   },
 });
